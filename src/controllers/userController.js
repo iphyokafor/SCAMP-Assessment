7100144bde226db1671e5164d573a2e78e67bb56
iphyokafor <sandraifeoma22@gmail.com>
2020-05-07 02:02:57 +0100

    Implemented user login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
-import express from 'express';
 import User from '../models/userModel';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import registerValidation from '../validation/registerValidation';
+import loginValidation from '../validation/loginValidation';
 
 export default {
     register: async(req, res) => {
@@ -29,14 +30,38 @@ export default {
 
         try {
             const savedUser = await user.save();
+            const {_id, firstName, lastName, email,role} = user
             res.send({
                 status: 201,
                 message: "User successfully registered",
-                user: user._id,
+                // user: user._id, 
+                user: {_id, firstName, lastName, email,role }
             });
 
         } catch (err) {
             res.status(400).json(err);
         }
     },
+
+    login: async(req, res) => {
+      const { error } = loginValidation(req.body);
+      if (error) return res.status(400).send(error.details[0].message);
+
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+          return res.status(400).send('Email doesn\'t exist');
+      }
+
+      const validPass = await bcrypt.compare(req.body.password, user.password);
+      if (!validPass) {
+          return res.status(400).send('Invalid Password');
+      }
+
+      const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+      res.header('auth-token', token).send({
+          status: 200,
+          token,
+          message: 'logged in'
+      });
+  }
 };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,5 +3,6 @@ import User from '../controllers/userController';
 const router = express.Router();
 
 router.post('/register', User.register);
+router.post('/login', User.login);
 
 export default router;
